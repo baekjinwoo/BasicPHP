94193697a73b5965062e5aba48bc2c1692193f6b
wlsdn3631 <wlsdn3631@gmail.com>
2019-02-07 02:25:01 +0900

    PHP의 문자

---
+++ string.php
@@ -0,0 +1,19 @@
+<h1> 1) String 문법 </h1>
+<h3>
+  <?php
+    //http://127.0.0.1/string.php
+    echo "Hello word_1";
+    echo 'Hello "w"ord_2';
+    echo "Hello \"w\"ord_3";
+  ?>
+<h3>
+
+<h1> 2) 결합연산자<h1>
+<?php
+  echo "Hello"."world";
+ ?>
+
+<h1> 3) 문자길이 <h1>
+<?php
+  echo strlen("Hello world");
+ ?>
