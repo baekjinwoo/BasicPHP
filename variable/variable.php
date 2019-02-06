143d3cdf878b3e0d99e4dbfaa840785535edbfd9
wlsdn3631 <wlsdn3631@gmail.com>
2019-02-07 02:25:33 +0900

    PHP의 변수

---
+++ variable/variable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1> Variable </h1>
+    <?php
+      //http://127.0.0.1/variable/variable.php
+      $a=10;
+      echo $a+1;
+
+      $name="egoing";
+      echo "<br>";
+      echo "Lorem ipsum dolor sit amet, consectetur " .$name." adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      " .$name. " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      " .$name. " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+     ?>
+  </body>
+</html>
