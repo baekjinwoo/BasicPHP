cebad240ac6b0c077ccf9122fc7f4e87f3144fe7
wlsdn3631 <wlsdn3631@gmail.com>
2019-02-07 02:35:21 +0900

    Boolean과 비교연산자

---
+++ comparison/comparison.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>비교연산자 &amp; Boolean Data Type</h1>
+    <?php
+      $br = "<br>";
+      var_dump(11);
+      echo $br;
+
+      var_dump('11');
+      echo $br;
+    ?>
+
+    <h2>1==1</h2>
+    <?php
+      var_dump(1==1);
+      echo $br;
+    ?>
+
+    <h2>1>1</h1>
+    <?php
+      var_dump(1>1);
+      echo $br;
+    ?>
+
+    <h2>1>=1</h1>
+    <?php
+      var_dump(1>=1);
+      echo $br;
+     ?>
+  </body>
+</html>
