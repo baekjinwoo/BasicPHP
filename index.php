28ebd6bf9dd4b4b2db99ed1efd1e0599c75f25c5
wlsdn3631 <wlsdn3631@gmail.com>
2019-02-07 02:23:44 +0900

    PHP의 숫자

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <?php
+      //생활코딩 PHP강의입니다.
+      //http://127.0.0.1/index.php
+      echo date('Y-m-d H:i:s');
+     ?>
+  </body>
+</html>
---
+++ number.php
@@ -0,0 +1,15 @@
+<?php
+  echo 1; // print(1);
+  echo 1+1;
+ ?>
+
+<!DOCTYPE html>
+ <html>
+  <body>
+    <h1>3</h1>
+    <?php
+      //http://127.0.0.1/number.php
+      echo 5-1; //print(3);
+    ?>
+  </body>
+</html>
