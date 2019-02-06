cc68012e44c8decea8f05e8b89b4909aa44bf0c8
wlsdn3631 <wlsdn3631@gmail.com>
2019-02-07 02:36:09 +0900

    조건문 형식

---
+++ comparison/conditional.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Conditional (조건문) </h1>
+    <h2> if </h2>
+    <?php
+      echo '1<br>';
+      if(true){  //Boolean타입
+        echo '2<br>';
+      }
+      else if(false){
+        echo '3<br>';
+      }
+
+
+
+    ?>
+  </body>
+</html>
